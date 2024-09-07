8fe984164c98e0af326ab60f65ddcb50bce12e64
Siddh8051 <siddharthkumar805100@gmail.com>
2024-09-07 13:25:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id =546431313
-api_hash = ""
-bot_token = "aaf4235465a4r3a1f3af13af1a4fa"
-auth_users = "46513214"
+api_id = 29029800
+api_hash = "0258b310e9ebb9b5155b5740d6ecf126"
+bot_token = "7251335061:AAH-s6BcjuaBoL8AMoMxbW4YIssKXvCKF7k"
+auth_users = "6790518589"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "56416564654"
+osowner_users = "6790518589"
 owner_users = [int(num) for num in osowner_users.split(",")]
